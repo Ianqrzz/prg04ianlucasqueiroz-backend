4a05a767913c227be2189edca6b4aa329c54938a
Ianqrzz <ianepeu22@gmail.com>
2026-07-02 19:12:28 -0300

    fix: disable Spring Security auto-configuration

--- src/main/java/br/com/ifba/Application.java
+++ src/main/java/br/com/ifba/Application.java
@@ -2,9 +2,9 @@ package br.com.ifba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
