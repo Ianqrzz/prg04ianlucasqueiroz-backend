6f0738d1fc1fe39108df981db8e6924acc576039
Ianqrzz <ianepeu22@gmail.com>
2026-07-02 19:32:54 -0300

    fix: remove Security auto-configuration exclusion and update CORS setup

--- src/main/java/br/com/ifba/Application.java
+++ src/main/java/br/com/ifba/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })public class Application {
+@SpringBootApplication public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/br/com/ifba/infraestructure/config/SecurityConfig.java
+++ src/main/java/br/com/ifba/infraestructure/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
